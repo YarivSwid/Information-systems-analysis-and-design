d6bbd25a6f763588f7da635e308d7f0e7f21985a
YarivSwid <yarivswid@gmail.com>
2021-12-24 01:29:11 +0200

    new sql files

---
+++ DropTables.sql
@@ -0,0 +1,12 @@
+drop table CUSTOMERBIDS
+drop table CONTACTS
+drop table CUSTOMERINVITS
+drop table USERINVITS
+drop table BUSINESSMEETINGS
+drop table CUSTOMERS
+drop table CHATPARTS
+drop table [MESSAGES]
+drop table CHATS
+drop table USERS
+
+drop table PAYTERMS
